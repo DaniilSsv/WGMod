914f4ec869b22fd6efc2a1ab14041128fffad756
DaniilSsv <daniil1samsonov@gmail.com>
2025-09-30 13:17:23 +0200

    start

--- src/main/java/com/Kaljmarik/WGKal/WgKalApplication.java
+++ src/main/java/com/Kaljmarik/WGKal/WgKalApplication.java
@@ -1,4 +1,4 @@
-package com.example.WGKal;
+package com.Kaljmarik.WGKal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/Kaljmarik/WGKal/WgKalApplicationTests.java
+++ src/test/java/com/Kaljmarik/WGKal/WgKalApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.WGKal;
+package com.Kaljmarik.WGKal;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
