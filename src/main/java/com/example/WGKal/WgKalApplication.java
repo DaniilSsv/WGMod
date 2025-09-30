129489ee3e76c040858d89f2db8af47252de6ce8
DaniilSsv <daniil1samsonov@gmail.com>
2025-09-30 12:36:26 +0200

    first c

---
+++ src/main/java/com/example/WGKal/WgKalApplication.java
@@ -0,0 +1,13 @@
+package com.example.WGKal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgKalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WgKalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WGKal/WgKalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WGKal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WgKalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
